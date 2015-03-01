fe9922f9015517f6e8877d0d871d92e7f5e2602d
wuyoubei1 <389234613@qq.com>
2015-03-01 09:03:30 +0800

    1

--- src/main/java/com/github/hzw/security/controller/SampleInputController.java
+++ src/main/java/com/github/hzw/security/controller/SampleInputController.java
@@ -1,10 +1,36 @@
 package com.github.hzw.security.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.github.hzw.security.entity.Resources;
+import com.github.hzw.util.Common;
 
 @Controller
 @RequestMapping("/background/sample/")
 public class SampleInputController extends BaseController {
-
+	/**
+	 * 列表页面
+	 * @param model
+	 * @param menu
+	 * @param pageNow
+	 * @return
+	 */
+	@RequestMapping("list")
+	public String list(Model model, Resources menu, String pageNow) {
+		return Common.BACKGROUND_PATH+"/sample/list";
+	}
+	
+	/**
+	 * 跳转到新增界面
+	 * 
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("addUI")
+	public String addUI(Model model) {
+		model.addAttribute("nowDate", Common.fromDateY());
+		return Common.BACKGROUND_PATH+"/sample/add";
+	}
 }
