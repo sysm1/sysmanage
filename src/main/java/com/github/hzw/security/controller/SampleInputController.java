efc79c3ef0a18c84f37a0d4ce960696053b82b14
wuyoubei1 <389234613@qq.com>
2015-03-01 22:53:55 +0800

    开版录入

--- src/main/java/com/github/hzw/security/controller/SampleInputController.java
+++ src/main/java/com/github/hzw/security/controller/SampleInputController.java
@@ -1,11 +1,5 @@
 package com.github.hzw.security.controller;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -18,8 +12,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.github.hzw.pulgin.mybatis.plugin.PageView;
 import com.github.hzw.security.entity.ClothInfo;
@@ -34,7 +26,7 @@ import com.github.hzw.security.service.SalesmanInfoService;
 import com.github.hzw.security.service.SampleInputService;
 import com.github.hzw.security.service.TechnologyInfoService;
 import com.github.hzw.util.Common;
-import com.github.hzw.util.PropertiesUtils;
+import com.github.hzw.util.UploadFileUtils;
 
 @Controller
 @RequestMapping("/background/sample/")
@@ -69,6 +61,14 @@ public class SampleInputController extends BaseController {
 	 */
 	@RequestMapping("list")
 	public String list(Model model, Resources menu, String pageNow) {
+		List<ClothInfo> cloths = clothInfoService.queryAll(null);
+		List<FactoryInfo> factoryInfos=factoryInfoService.queryAll(null);
+		List<TechnologyInfo> technologyInfos= technologyInfoService.queryAll(null);
+		List<SalesmanInfo> salesmanInfos= salesmanInfoService.queryAll(null);
+		model.addAttribute("cloths", cloths);
+		model.addAttribute("salesmanInfos", salesmanInfos);
+		model.addAttribute("factoryInfos", factoryInfos);
+		model.addAttribute("technologyInfos", technologyInfos);
 		return Common.BACKGROUND_PATH+"/sample/list";
 	}
 	
@@ -79,8 +79,9 @@ public class SampleInputController extends BaseController {
 	 */
 	@ResponseBody
 	@RequestMapping("query")
-	public PageView query(SampleInput sampleInput,String pageNow,String pagesize) {
+	public PageView query(SampleInput sampleInput,String pageNow,String pagesize,Model model) {
 		pageView = sampleInputService.query(getPageView(pageNow,pagesize), sampleInput);
+		model.addAttribute("bean", sampleInput);
 		return pageView;
 	}
 	
@@ -113,44 +114,8 @@ public class SampleInputController extends BaseController {
 	@RequestMapping("add")
 	@ResponseBody
 	public Map<String, Object> add(HttpServletRequest request,SampleInput sampleInput) {
-		MultipartHttpServletRequest multipartRequest  =  (MultipartHttpServletRequest) request;  
-        //  获得第1张图片（根据前台的name名称得到上传的文件）   
-        MultipartFile myFile =  multipartRequest.getFile("myFile");
-        final int BUFFER_SIZE = 16 * 1024 ;
-        OutputStream out = null ;
-        InputStream in=null;
-        File  dst=null;
-        //图片保存目标路径
-        String dstPath=PropertiesUtils.findPropertiesKey("pic_path");
-        File path=new File(dstPath);
-        if(!path.exists()){
-        	path.mkdirs();
-        }
-        String picName=myFile.getOriginalFilename();
-        dst=new File(dstPath+picName);
-        sampleInput.setPicture(dstPath+picName);
-        try {
-        	in =myFile.getInputStream();
-        	out = new BufferedOutputStream( new FileOutputStream(dst), BUFFER_SIZE);
-        	byte [] buffer = new byte [BUFFER_SIZE];
-        	while (in.read(buffer) > 0 ) {
-        		out.write(buffer);
-        	}
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}finally{
-			try {
-				if ( null != in) {
-				in.close();
-				}
-				if ( null != out) {
-					out.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-        
+		String picPath=UploadFileUtils.saveUploadFile(request);
+		sampleInput.setPicture(picPath);
 		Map<String, Object> map = new HashMap<String, Object>();
 		try {
 			sampleInput.setStatus(0);
@@ -179,7 +144,7 @@ public class SampleInputController extends BaseController {
 	 * @return
 	 */
 	@RequestMapping("editUI")
-	public String editUI(Model model,String id) {
+	public String editUI(Model model,String id,HttpServletRequest request ) {
 		SampleInput sampleInput = sampleInputService.getById(id);
 		List<ClothInfo> cloths = clothInfoService.queryAll(null);
 		List<FactoryInfo> factoryInfos=factoryInfoService.queryAll(null);
@@ -190,6 +155,61 @@ public class SampleInputController extends BaseController {
 		model.addAttribute("factoryInfos", factoryInfos);
 		model.addAttribute("technologyInfos", technologyInfos);
 		model.addAttribute("bean", sampleInput);
+		model.addAttribute("copyadd", request.getParameter("copyadd"));
 		return Common.BACKGROUND_PATH+"/sample/edit";
+	}
+	/**
+	 * 更新开版录入 wuyb
+	 */
+	@ResponseBody
+	@RequestMapping("update")
+	public Map<String, Object> update(Model model, SampleInput sampleInput,HttpServletRequest request) {
+		String copyadd= request.getParameter("copyadd");
+		Map<String, Object> map = new HashMap<String, Object>();
+		try {
+			if("copyadd".equals(copyadd)){//如果不为空  则此数据为复制新增的数据
+				sampleInput.setId(null);
+				String picPath=UploadFileUtils.saveUploadFile(request);
+				sampleInput.setPicture(picPath);
+				sampleInput.setStatus(0);
+				sampleInput.setCreateTime(new Date());
+				int codeType=sampleInput.getCodeType();
+				if(codeType==0){
+					sampleInput.setFileCode(sampleInput.getCodeValue());
+				}else if(codeType==1){
+					sampleInput.setFactoryCode(sampleInput.getCodeValue());
+				}else if(codeType==2){
+					sampleInput.setMyCompanyCode(sampleInput.getCodeValue());
+				}
+				sampleInputService.add(sampleInput);
+			}else{
+				sampleInputService.update(sampleInput);
+			}
+			map.put("flag", "true");
+		} catch (Exception e) {
+			map.put("flag", "false");
+			e.printStackTrace();
+		}
+		return map;
+	}
+	/**
+	 * 删除 wuyb
+	 */
+	@ResponseBody
+	@RequestMapping("deleteById")
+	public Map<String, Object> deleteById(Model model, String ids) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		try {
+			String id[] = ids.split(",");
+			for (String string : id) {
+				if(!Common.isEmpty(string)){
+					sampleInputService.delete(string);
+				}
+			}
+			map.put("flag", "true");
+		} catch (Exception e) {
+			map.put("flag", "false");
+		}
+		return map;
 	}
 }
