e0f7eadcef43732e93be1d5f9fdf56ee4e389748
hujh.deng@gmail.com <hujh@localhost>
2015-03-01 12:35:46 +0800

    作为序号生产器使用

---
+++ src/main/java/com/github/hzw/security/mapper/DateVersionMapper.java
@@ -0,0 +1,8 @@
+package com.github.hzw.security.mapper;
+
+import com.github.hzw.base.BaseMapper;
+import com.github.hzw.security.entity.DateVersion;
+
+public interface DateVersionMapper extends BaseMapper<DateVersion> {
+
+}
