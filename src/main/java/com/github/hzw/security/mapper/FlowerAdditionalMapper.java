329f60f7945c20b32d85db5128a1f263242480cc
wuyoubei1 <389234613@qq.com>
2015-03-29 09:00:28 +0800

    12

--- src/main/java/com/github/hzw/security/mapper/FlowerAdditionalMapper.java
+++ src/main/java/com/github/hzw/security/mapper/FlowerAdditionalMapper.java
@@ -10,4 +10,18 @@ public interface FlowerAdditionalMapper extends BaseMapper<FlowerAdditional>{
 	public void deleteByFlowerId(String flowerId);
 	
 	public List<FlowerAdditional> queryFind(FlowerAdditional fa);
+	
+	/**
+	 * 根据我司编号 查询工厂编号
+	 * @param myCompanyCode
+	 * @return
+	 */
+	public List<String> queryFactoryCode(String  myCompanyCode);
+	
+	/**
+	 * 根据我司颜色 查询工厂颜色
+	 * @param myCompanyColor
+	 * @return
+	 */
+	public List<String> queryFactoryColor(String  myCompanyColor);
 }
--- src/main/java/com/github/hzw/security/mapper/ReturnGoodsProcessMapper.java
+++ src/main/java/com/github/hzw/security/mapper/ReturnGoodsProcessMapper.java
@@ -1,10 +1,22 @@
 package com.github.hzw.security.mapper;
 
+import java.util.List;
+import java.util.Map;
+
 import com.github.hzw.base.BaseMapper;
+import com.github.hzw.security.VO.OrderSummaryVO;
 import com.github.hzw.security.entity.ReturnGoodsProcess;
 
 public interface ReturnGoodsProcessMapper extends
 		BaseMapper<ReturnGoodsProcess> {
+	
+	/**
+	 * 返回分页后的数据
+	 * @param List
+	 * @param t
+	 * @return
+	 */
+	public List<OrderSummaryVO> queryVO(Map<String, Object> map);
 
 	
 }
