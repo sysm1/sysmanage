29f90ae4888fdaa03a734326f75b0dfe323d631c
wuyoubei1 <389234613@qq.com>
2015-03-29 23:07:48 +0800

    222

--- src/main/java/com/github/hzw/security/mapper/SampleInputMapper.java
+++ src/main/java/com/github/hzw/security/mapper/SampleInputMapper.java
@@ -12,5 +12,12 @@ public interface SampleInputMapper extends BaseMapper<SampleInput>{
 	public List<SampleInput>  queryReplay(Map<String, Object> map);
 	
 	public List<SampleInputVO> queryXinBan(Map<String, Object> map);
+	
+	/**
+	 * 查询拖延单数
+	 * @param dates
+	 * @return
+	 */
+	public String queryDelayDates(String dates);
 
 }
